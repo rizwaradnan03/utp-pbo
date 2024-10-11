1e46d215629be2fb3e9b68ba691f49d15d4bbe71
riswan6969 <riswan146lubis@gmail.com>
2024-10-12 02:41:30 +0700

    Membuat Class dokter dan menambahkan Objeknya

--- lib/Dokter.dart
+++ lib/Dokter.dart
@@ -1,3 +1,17 @@
-class Dokter{
-  int? id;
+class Dokter {
+  String? nama;
+  int? NoHp;
+
+  Dokter(String nama, int NoTelepon) {
+    this.nama = nama;
+    this.NoHp = NoTelepon;
+  }
+  void identitasDokter() {
+    print("NamaDokter $nama, Dengan NO HP $NoHp");
+  }
+}
+
+void main() {
+  Dokter dokter1 = Dokter("Riswan M.BD", 087462454325);
+  dokter1.identitasDokter();
 }
