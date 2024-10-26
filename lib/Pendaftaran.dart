a87c03ec1f43eff13f09c99936a80c9b4a3a1529
RISKYR1212 <rizkyrispaldi0@gmail.com>
2024-10-26 21:07:20 +0700

    pendaftaran

---
+++ lib/Pendaftaran.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+
+import 'Dokter.dart';
+import 'Pasien.dart';
+import 'Penjadwalan.dart';
+
+class Pendaftaran {
+  List < Dokter > dokter = [];
+  List < Pasien > pasien = [];
+void daftarDokter (Dokter dokter){
+  this.dokter.add(dokter);
+  dokter.daftar();
+  
+}
+void daftarpasien (Pasien pasien) {
+  this.pasien.add(pasien);
+  pasien.daftar();
+}
+void penjadwalan (Dokter dokter,Jadwal jadwal){
+dokter.tambahJadwal(jadwal);
+}
+}
