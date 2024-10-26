e39dcd5f211776fba825e17dd71d290ab562b199
Rizwar Adnanbyantara <rizwarbyan123@gmail.com>
2024-10-26 20:24:58 +0700

    menambahkan jadwal

---
+++ lib/Penjadwalan.dart
@@ -0,0 +1,11 @@
+class Jadwal {
+  String? hari;
+  DateTime? waktuMulai;
+  DateTime? waktuSelesai;
+
+  Jadwal(String hari, DateTime waktuMulai, DateTime waktuSelesai){
+    this.hari = hari;
+    this.waktuMulai = waktuMulai;
+    this.waktuSelesai = waktuSelesai;
+  }
+}
